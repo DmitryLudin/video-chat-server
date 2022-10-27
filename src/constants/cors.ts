b64bf8e8ac0819e61fb154900e28566864e54790
Dmitry Ludin <ludin@tochka.com>
2022-10-27 22:35:07 +0500

    build(app): cors

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -1,5 +1,6 @@
 export const cors = {
-  origin: ['http://localhost:3000', 'http://62.84.122.93'],
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   credentials: true,
   allowedHeaders: ['Accept', 'Content-Type'],
 };
