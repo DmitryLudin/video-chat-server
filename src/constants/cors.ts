11b9b2c71ec5b9613e2f59c8a77d85a5e8ba3a3b
Dmitry Ludin <ludin@tochka.com>
2022-10-28 13:02:39 +0500

    build(app): update cors

--- src/constants/cors.ts
+++ src/constants/cors.ts
@@ -4,6 +4,8 @@ export const cors = {
     'http://62.84.122.93',
     'http://ludind-video-conf.ru',
     'http://api.ludind-video-conf.ru',
+    'https://ludind-video-conf.ru',
+    'https://api.ludind-video-conf.ru',
   ],
   credentials: true,
   allowedHeaders: ['Accept', 'Content-Type'],
